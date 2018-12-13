7915aaee87685b3a50e8187cfab6248c4a56fcc7
Nia Tahl <eliyanbrize@gmail.com>
2018-12-13 18:39:59 +0100

    Added Takumi core

--- jars/src/data/scripts/hullmods/SRD_EccentricCoreCieve.java
+++ jars/src/data/scripts/hullmods/SRD_EccentricCoreCieve.java
@@ -27,7 +27,6 @@ public class SRD_EccentricCoreCieve extends BaseHullMod {
 
     //Normal Eccentric stats
     public static final float SUPPLY_USE_MULT = 1.50f;
-    public static final float CREW_CAPACITY_MULT = 0.8f;
 
     //Special stats for Cieve
     public static final float BALLISTIC_FLUX_USE_MULT = 0.9f;
@@ -43,7 +42,6 @@ public class SRD_EccentricCoreCieve extends BaseHullMod {
     public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
         stats.getSuppliesPerMonth().modifyMult(id, SUPPLY_USE_MULT);
 
-        stats.getMaxCrewMod().modifyMult(id, CREW_CAPACITY_MULT);
         stats.getMinCrewMod().modifyMult(id, 0f);
 
         stats.getDamageToMissiles().modifyPercent(id, BONUS_PD_DAMAGE_PERCENT);
@@ -70,18 +68,17 @@ public class SRD_EccentricCoreCieve extends BaseHullMod {
     //Adds the description strings
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0) return "" + (int)((SUPPLY_USE_MULT-1f)*100f) + "%";
-        if (index == 1) return "" + (int)Math.ceil((CREW_CAPACITY_MULT-1f)*-100f) + "%";
-        if (index == 2) return "Sylph Core";
+        if (index == 1) return "Sylph Core";
         if (Global.getSector() != null && Global.getCurrentState().equals(GameState.CAMPAIGN)) {
-            if (index == 3 && Global.getSector().getFaction(Factions.PLAYER).getMemoryWithoutUpdate().contains("$SRDHasSeenAIRebellion")) return " Has a chance to rebel under certain circumstances.";
-            else if (index == 3) return "";
-        } else if (index == 3) {
+            if (index == 2 && Global.getSector().getFaction(Factions.PLAYER).getMemoryWithoutUpdate().contains("$SRDHasSeenAIRebellion")) return " Has a chance to rebel under certain circumstances.";
+            else if (index == 2) return "";
+        } else if (index == 2) {
             return "";
         }
-        if (index == 4) return "-Cieve";
-        if (index == 5) return "" + (int)Math.ceil((BALLISTIC_FLUX_USE_MULT-1f)*-100f) + "%";
-        if (index == 6) return "" + (int)Math.round(BONUS_PD_DAMAGE_PERCENT) + "%";
-        if (index == 7) return "perfect target leading";
+        if (index == 3) return "-Cieve";
+        if (index == 4) return "" + (int)Math.ceil((BALLISTIC_FLUX_USE_MULT-1f)*-100f) + "%";
+        if (index == 5) return "" + (int)Math.round(BONUS_PD_DAMAGE_PERCENT) + "%";
+        if (index == 6) return "perfect target leading";
         return null;
     }
 }
--- jars/src/data/scripts/hullmods/SRD_EccentricCoreTakumi.java
+++ jars/src/data/scripts/hullmods/SRD_EccentricCoreTakumi.java
@@ -12,18 +12,20 @@ import data.scripts.SRD_ModPlugin;
 
 import java.util.ArrayList;
 
-public class SRD_EccentricCoreBalance extends BaseHullMod {
+public class SRD_EccentricCoreTakumi extends BaseHullMod {
     private static final String BAD_HULLMOD_NOTIFICATION_SOUND = "cr_allied_critical";
+
+    private final float CHECK=1f;
+    private float timer=0, previous=0;
+    private String ID="Booster Overdrive";
 
     //Normal Eccentric stats
     public static final float SUPPLY_USE_MULT = 1.50f;
 
-    //Balance specific stats
-    public static float SHIELD_DEPLOY_BOOST = 2f;
-    public static float CP_RECOVERY_BOOST = 2f;
+    //Takumi specific stats
+    public static float ENGINE_DAMAGE_MULT = 0.1f;
 
-    //Fleet buff
-    public static float FLEET_RANGE_BOOST = 200f;
+
 
     //-----------------------------------------------------Betrayal stats...--------------------------------------------
     //Bonus multiplier for "percieved intelligence", for the purpose of Sylph Core betrayal. It's an Eccentric, so
@@ -37,36 +39,9 @@ public class SRD_EccentricCoreBalance extends BaseHullMod {
 
         stats.getMinCrewMod().modifyMult(id, 0f);
 
-        stats.getShieldUnfoldRateMult().modifyMult(id, SHIELD_DEPLOY_BOOST);
-        stats.getBallisticWeaponRangeBonus().modifyFlat(id, FLEET_RANGE_BOOST);
-        stats.getEnergyWeaponRangeBonus().modifyFlat(id,FLEET_RANGE_BOOST);
-
+        stats.getEngineDamageTakenMult().modifyMult(id, ENGINE_DAMAGE_MULT);
     }
 
-    public void applyEffectsAfterShipCreation(ShipAPI ship, String id) {
-        //CR recovery bonus
-        ship.getMutableStats().getDynamic().getMod(Stats.COMMAND_POINT_RATE_FLAT).modifyFlat(id, CP_RECOVERY_BOOST);
-
-        //---For incompatible hullmods---
-        //Creates a list for later use
-        ArrayList<String> deletionList = new ArrayList<String>();
-
-        //Checks if any given hullmods is a shield-related hullmod
-        for (String s : ship.getVariant().getNonBuiltInHullmods()) {
-            if (SRD_ModPlugin.SHIELD_HULLMODS.contains(s)) {
-                deletionList.add(s);
-            }
-        }
-
-        //Finally, deletes the hullmods we aren't allowed to have
-        if (deletionList.size() > 0) {
-            ship.getVariant().addMod("SRD_IncompatibleHullmodWarning");
-            Global.getSoundPlayer().playUISound(BAD_HULLMOD_NOTIFICATION_SOUND, 1f, 1f);
-        }
-        for (String s : deletionList) {
-            ship.getVariant().removeMod(s);
-        }
-    }
 
     //Applies any in-combat mutable effects
     @Override
@@ -74,11 +49,29 @@ public class SRD_EccentricCoreBalance extends BaseHullMod {
         //Handles the betrayal part of the code by accessing the Sylph Core's code; no need to duplicate
         SRD_SylphCore.advanceInCombatStatic(ship,amount);
 
-        //Fleetwide range boost for Sylph Core equipped allies
-        for (ShipAPI otherShip : Global.getCombatEngine().getShips()) {
-            if (otherShip.getVariant().getHullMods().contains("SRD_sylph_core") && !otherShip.isHulk() && otherShip.getOwner() == ship.getOwner()) {
-                otherShip.getMutableStats().getBallisticWeaponRangeBonus().modifyFlat("SRD_EccentricCoreBalanceID", FLEET_RANGE_BOOST);
-                otherShip.getMutableStats().getEnergyWeaponRangeBonus().modifyFlat("SRD_EccentricCoreBalanceID",FLEET_RANGE_BOOST);
+        timer=Global.getCombatEngine().getTotalElapsedTime(false);
+
+        ShipAPI playerShip = Global.getCombatEngine().getPlayerShip();
+        if(previous>=timer || timer>previous+CHECK || ship==playerShip){
+
+            if(ship!=playerShip){
+                previous=timer;
+            }
+
+            int tick = (int)ship.getFullTimeDeployed()/30;
+            float effectLevel = Math.min(1, 0.2f*tick);
+
+            ship.getMutableStats().getMaxSpeed().modifyMult(ID, 1+(50*effectLevel/100));
+            ship.getMutableStats().getAcceleration().modifyMult(ID, 1+(50*effectLevel/100));
+            ship.getMutableStats().getDeceleration().modifyMult(ID, 1+(50*effectLevel/100));
+
+            if(ship==playerShip){
+                Global.getCombatEngine().maintainStatusForPlayerShip(
+                        "TakumiOverdrive",
+                        "graphics/icons/hullsys/high_energy_focus.png",
+                        "Booster Overdrive",
+                        "Running at "+Math.round(effectLevel*100)+"% power",
+                        effectLevel < 0f);
             }
         }
     }
@@ -99,12 +92,11 @@ public class SRD_EccentricCoreBalance extends BaseHullMod {
         } else if (index == 2) {
             return "";
         }
-        if (index == 3) return "-Balance Unto All";
-        if (index == 4) return "" + (int)SHIELD_DEPLOY_BOOST + " times";
-        if (index == 5) return "100%";
-        if (index == 6) return "non-eccentric";
-        if (index == 7) return "200su";
-        if (index == 8) return "unable to mount any shield-related hullmods";
+        if (index == 3) return "-Takumi";
+        if (index == 4) return "90%";
+        if (index == 5) return "10%";
+        if (index == 6) return "30 seconds";
+        if (index == 7) return "50%";
         return null;
     }
 }
