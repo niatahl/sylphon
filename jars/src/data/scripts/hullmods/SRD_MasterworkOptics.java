ccfcc775c7ff4db5a0c5e9447c12bae26af5674b
Nia Tahl <eliyanbrize@gmail.com>
2018-12-13 14:55:22 +0100

    Added bombardment bonus to Finis Astra and vernier thrusters to Replekia

--- jars/src/data/scripts/hullmods/SRD_MasterworkOptics.java
+++ jars/src/data/scripts/hullmods/SRD_MasterworkOptics.java
@@ -3,6 +3,7 @@ package data.scripts.hullmods;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.impl.campaign.ids.Stats;
 import data.scripts.SRD_ModPlugin;
 import org.lazywizard.lazylib.FastTrig;
 import org.lazywizard.lazylib.MathUtils;
@@ -22,6 +23,7 @@ public class SRD_MasterworkOptics extends BaseHullMod {
     public static float BEAM_RANGE_INCREASE = 400f;
     public static float SUPPLY_USE_MULT = 2.25f;
     public static float REPAIR_COST_MULT = 1.5f;
+    public static float BOMBARD_BONUS = 500f;
 
     //Applies the effects
     @Override
@@ -29,6 +31,7 @@ public class SRD_MasterworkOptics extends BaseHullMod {
         stats.getBeamWeaponRangeBonus().modifyFlat(id, BEAM_RANGE_INCREASE);
         stats.getSuppliesPerMonth().modifyMult(id, SUPPLY_USE_MULT);
         stats.getSuppliesToRecover().modifyMult(id, REPAIR_COST_MULT);
+        stats.getDynamic().getMod(Stats.FLEET_BOMBARD_COST_REDUCTION).modifyFlat("SRD_MasterworkOpticsID", BOMBARD_BONUS);
     }
 
     //Never applicable: only comes built-in
@@ -40,8 +43,9 @@ public class SRD_MasterworkOptics extends BaseHullMod {
     //Adds the description strings
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0) return "" + (int)BEAM_RANGE_INCREASE;
-        if (index == 1) return "" + (int)((SUPPLY_USE_MULT-1f)*100f) + "%";
-        if (index == 2) return "" + (int)((REPAIR_COST_MULT-1f)*100f) + "%";
+        if (index == 1) return "" + (int)BOMBARD_BONUS + " units of fuel";
+        if (index == 2) return "" + (int)((SUPPLY_USE_MULT-1f)*100f) + "%";
+        if (index == 3) return "" + (int)((REPAIR_COST_MULT-1f)*100f) + "%";
         return null;
     }
 }
